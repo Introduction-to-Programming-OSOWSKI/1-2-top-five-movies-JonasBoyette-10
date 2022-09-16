45a47c75f48cb758e8691f7031c1828bb4a5ef60
JonasBoyette-10 <81956@mystma.org>
2022-09-16 14:39:17 -0500

    2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(a, b, c, d, e):
     return a + " " + b + " " + c + " " + d + " " + e
 
-print(topFiveMovies("1", "2", "3", "4", "5"))
+print(topFiveMovies("LotR RotK", "LotR Two Towrs", "LotR FotR ", "The Hobbit", "Planet Hulk"))
